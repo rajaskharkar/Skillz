7bbfd91c12154b281f8a7d71dfbd034498fcdf9e
Rajas Kharkar <rajaskharkar278@gmail.com>
2025-12-16 19:10:24 -0600

    Moving colors to correct file

--- app/src/main/java/com/kingkharnivore/skillz/ui/theme/Color.kt
+++ app/src/main/java/com/kingkharnivore/skillz/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GryffindorRed =  Color(0xFF7F0909)
+val RavenclawBlue =  Color(0xFF2F4F6F)
+val SteelGrey =  Color(0xFF3A4652)
+val SageMist = Color(0xFF7E9B8B)
+val SlytherinButNiceTeal = Color(0xFF3F8F8B)
+val AntiqueGold = Color(0xFFB8A56A)     // Gold
+val Bronze = Color(0xFFB7893A)
+val GryffindorOffWhite = Color(0xFFF2EBDD) // Parchment
+val GryffindorBlack = Color(0xFF000000)
