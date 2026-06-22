9331a63dd62ee448958867158c5e24529d0ad2ee
Rajas Kharkar <rajaskharkar278@gmail.com>
2026-06-22 15:49:53 -0500

    Clean up iOS app skeleton

--- ios/Scyra/Scyra/ScyraApp.swift
+++ ios/Scyra/Scyra/ScyraApp.swift
@@ -1,32 +1,12 @@
-//
-//  ScyraApp.swift
-//  Scyra
-//
-//  Created by Rajas Kharkar on 6/20/26.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
 struct ScyraApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    private let container = AppDependencyContainer()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppRootView(container: container)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
