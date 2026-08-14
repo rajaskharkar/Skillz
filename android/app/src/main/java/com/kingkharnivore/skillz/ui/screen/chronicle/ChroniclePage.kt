524083441004881686a968595ec92720efc8f411
Rajas Kharkar <rajaskharkar278@gmail.com>
2026-08-13 22:20:23 -0500

    Fix Chronicle media alignment import

--- android/app/src/main/java/com/kingkharnivore/skillz/ui/screen/chronicle/ChroniclePage.kt
+++ android/app/src/main/java/com/kingkharnivore/skillz/ui/screen/chronicle/ChroniclePage.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
